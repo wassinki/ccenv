29190b23a31b59793213b974de296c76fb2bb75e
Ingo Wassink <ingo.wassink@nedap.com>
2011-04-28 13:15:50 +0200

    selenium only loaded if needed

--- vendor/plugins/cucumber_environment/lib/cucumber_environment/support/selenium_session_helpers.rb
+++ vendor/plugins/cucumber_environment/lib/cucumber_environment/support/selenium_session_helpers.rb
@@ -1,11 +1,10 @@
 module SeleniumSessionHelpers     
-  DEFAULT_TIMEOUT = 30   
   
   # Method to wait for the elemens
   def wait_for_load(hints = {})
     begin          
       if hints[:url]
-       selenium.wait_for_page(DEFAULT_TIMEOUT)
+       selenium.wait_for_page(30)
       elsif hints[:link]
         sleep(2)
       end
